67d389fec1b5145f043a461a22bb437f644aa31b
Ashish79924 <kumarashish79924@gmail.com>
2024-12-06 00:16:35 +0530

    test case handle to pass string input and output will the sum of a character

--- TDD_ASSIGNMENT/tdd/string_calculator.rb
+++ TDD_ASSIGNMENT/tdd/string_calculator.rb
@@ -1,5 +1,8 @@
 class StringCalculator
     def add(numbers)
         return 0 if numbers.empty?
+        number_array = numbers.split(",")
+        integers = number_array.map(&:to_i)
+        integers.sum
     end
 end
--- TDD_ASSIGNMENT/tdd/string_calculator_test.rb
+++ TDD_ASSIGNMENT/tdd/string_calculator_test.rb
@@ -11,4 +11,14 @@ class StringCalculatorTest < Minitest::Test
     def test_empty_string_returns_zero
       assert_equal 0, @calculator.add("")
     end
+
+    # Test case single number input
+    def test_single_number_returns_same_number
+      assert_equal 1, @calculator.add("1")
+    end
+
+    # Test case multiple numbers separated by commas
+    def test_multiple_numbers_sum
+      assert_equal 6, @calculator.add("1,2,3")
+    end
 end
